cf5064edce8f5f48358586c907e3b93c7baeb121
Stiven <stivengoldrin15@gmail.com>
2024-11-03 21:55:26 +0200

    Initial commit

---
+++ sg23054_main.py
@@ -0,0 +1,8 @@
+# This is a simple calculation program
+# created to demonstrate version control.
+from sg23054_libadd import *
+from sg23054_libmult import *
+a = 13
+b = 5
+print(plus(a,b))
+print(mult(a,b))
